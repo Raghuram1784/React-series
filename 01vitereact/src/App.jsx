fe1750aac9a8bd708598c1591a03ed4563e7dca8
Raghuram1784 <ymvraghu1784@gmail.com>
2024-07-29 13:12:43 +0530

    ViteReact

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+// import Chai from "./chai"
+
+function App() {
+  const username = "React series"
+  
+  return (
+    <>
+    {/* <Chai/> */}
+    <h1>chai aur code {username}</h1>
+    <p>paragrapgh</p>
+    </>
+  )
+}
+
+export default App
